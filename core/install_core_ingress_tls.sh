28fdd687ff4a98617acf403ca0f41ffdcf8288db
stubrowncloudbees <sbrown@cloudbees.com>
2019-11-01 15:11:01 +0000

    some changes to ingress

--- core/install_core_ingress_tls.sh
+++ core/install_core_ingress_tls.sh
@@ -1,2 +1,2 @@
-helm install --name cloudbees-core --set OperationsCenter.HostName='demo.k8s.stubrown.net' --set OperationsCenter.Ingress.tls.Enable=true --set nginx-ingress.Enabled=true --set OperationsCenter.ServiceType='ClusterIP' --set OperationsCenter.Ingress.tls.SecretName='cloudbees-core-example-com-tls' --namespace='cloudbees-core'  cloudbees/cloudbees-core
+helm install --name cloudbees-core --set Persistence.StorageClass='nfs-client' --set OperationsCenter.HostName='demo.k8s.stubrown.net' --set OperationsCenter.Ingress.tls.Enable=true --set nginx-ingress.Enabled=true --set OperationsCenter.ServiceType='ClusterIP' --set OperationsCenter.Ingress.tls.SecretName='cloudbees-core-stubrown-tls' --namespace='cloudbees-core'  cloudbees/cloudbees-core
 
